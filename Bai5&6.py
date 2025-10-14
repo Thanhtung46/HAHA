d7180be3fe16d82ccda1cca81e57c477a4c464b9
TruogKhoa <dtkhoa2411@clc.fitus.edu.vn>
2025-10-14 14:41:23 +0700

    Bai5&6

--- Bai5&6.py
+++ Bai5&6.py
@@ -1,8 +1,8 @@
 with open("output.txt", "w", encoding="utf-8") as f:
     f.write("I'm a student.\n")
     f.write(f"{1/7:.5f}\n")
-    a = 3
-    b = 4
+    a = 1
+    b = 1
     f.write(f"The sum of {a} and {b} is: {a + b}\n")
 
 with open("output.txt", "ab") as f:

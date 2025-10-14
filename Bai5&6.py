517727427e2e7d625fdb97f12ac814741283c7b8
TruogKhoa <dtkhoa2411@clc.fitus.edu.vn>
2025-10-14 14:37:26 +0700

    Bai5&6

---
+++ Bai5&6.py
@@ -0,0 +1,10 @@
+with open("output.txt", "w", encoding="utf-8") as f:
+    f.write("I'm a student.\n")
+    f.write(f"{1/7:.5f}\n")
+    a = 3
+    b = 4
+    f.write(f"The sum of {a} and {b} is: {a + b}\n")
+
+with open("output.txt", "ab") as f:
+    text = "Hôm nay trời đẹp."
+    f.write(text.encode("utf-8"))
